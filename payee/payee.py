a47d0209f523bda3b8bcacf2f0c536541b572d06
HollyMollyHollyMolly <dnuyen9@academic.rrc.ca>
2025-10-15 19:50:47 -0500

    payee update

--- payee/payee.py
+++ payee/payee.py
@@ -1,14 +1,13 @@
 """This module defines the Payee enumeration."""
 
 __author__ = "ACE Faculty"
-__version__ = "10.8git.2025"
+__version__ = "10.15.2025"
 __credits__ = "Nguyen Dang Thai Ha"
 
 from enum import Enum
 
 class Payee(Enum):
-    """Represents the receiver of a payment."""
-
+    
     ELECTRICITY = 1
     """The electricity payee."""
     

b25796c9288c0b6789cba6cca6842879668fe4fa
HollyMollyHollyMolly <dnuyen9@academic.rrc.ca>
2025-10-15 19:38:40 -0500

    partial payment strategy

--- patterns/strategy/partial_payment_strategy.py
+++ patterns/strategy/partial_payment_strategy.py
@@ -1,4 +1,16 @@
 """This module defines the PartialPaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Nguyen Dang Thai Ha"
+__version__ = "10.08.2025"
+
+from .payment_strategy import PaymentStrategy
+
+class PartialPaymentStrategy(PaymentStrategy):
+    def process_payment(self, account, payee, amount: float) -> str:
+        account.deduct_balance(payee, amount)
+        balance = account.get_balance(payee)
+
+        if balance <=0:
+            return f"Processed payment of ${amount:.2f}. New balance: ${balance:.2f}."
+        else:
+            return f"Partial payment of ${amount:.2f} accepted. New balance: ${balance:.2f}."
--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,15 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Nguyen Dang Thai Ha"
+__version__ = "10.08.2025"
+
+from abc import ABC, abstractmethod
+from payee.payee import Payee
+
+class PaymentStrategy(ABC):
+    """Abstract base class for payment strategies."""
+
+    @abstractmethod
+    def pay(self, amount: float, payee: Payee) -> None:
+        """Process a payment of the specified amount to the given payee."""
+        pass

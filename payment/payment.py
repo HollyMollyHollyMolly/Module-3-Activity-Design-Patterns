165292a5a02518167b05e604a0b46b4abed30ade
HollyMollyHollyMolly <dnuyen9@academic.rrc.ca>
2025-10-15 19:52:15 -0500

    update payment

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,18 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Nguyen Dang Thai Ha"
+__version__ = "10.15.2025"
 
 from billing_account.billing_account import BillingAccount
+from patterns.strategy.payment_strategy import PaymentStrategy
 from payee.payee import Payee
+
+class Payment:
+    def __init__(self, strategy):
+        if isinstance(strategy, PaymentStrategy):
+            self.__strategy = strategy
+        else:
+            raise ValueError("Invalid Strategy")
+    def pay_bill(self, account: BillingAccount, payee: Payee, amount: float) -> str:
+        return self.__strategy.process_payment(account, payee, amount)
+    

dcc7cc891a6ec2e7e2d9982ac9d09d141b262321
HollyMollyHollyMolly <dnuyen9@academic.rrc.ca>
2025-10-17 16:46:51 -0500

    update partial_payment doc string

--- patterns/strategy/partial_payment_strategy.py
+++ patterns/strategy/partial_payment_strategy.py
@@ -1,15 +1,30 @@
 """This module defines the PartialPaymentStrategy class."""
-
+ 
 __author__ = "Nguyen Dang Thai Ha"
 __version__ = "10.08.2025"
-
+ 
 from .payment_strategy import PaymentStrategy
-
+ 
 class PartialPaymentStrategy(PaymentStrategy):
+    """
+    Initialize a PartialPaymentStrategy instance.
+ 
+    Raises: None
+    """
     def process_payment(self, account, payee, amount: float) -> str:
+        """Apply the amount paid to the payee with Partial payment strategy.
+       
+        Args:
+            account: Account object
+            payee(Enum): Type of payee
+            amount(float): The amount of payment
+           
+        Returns:
+            str: A string with anoucement when use process_payment of partial payment strategy
+        """
         account.deduct_balance(payee, amount)
         balance = account.get_balance(payee)
-
+ 
         if balance <=0:
             return f"Processed payment of ${amount:.2f}. New balance: ${balance:.2f}."
         else:
